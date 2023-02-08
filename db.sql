4bb0a200663bd0f8ed30a3d68f32b0eb400cc01e
Sohaib Ali <sohaib.saim.ali@gmail.com>
2023-02-09 03:21:15 +0500

    Updated DB File

--- db.sql
+++ db.sql
@@ -28,7 +28,7 @@ CREATE TABLE `account` (
   `email` varchar(500) DEFAULT NULL,
   `password` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,7 +57,7 @@ CREATE TABLE `customer` (
   `todaysPickup` varchar(500) DEFAULT NULL,
   `meals` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
